15ccd79eeac439cc0dd233ef479e03a5f02b8ad7
Miles <milescollins9@gmail.com>
2025-06-10 14:56:13 -0500

    added metadatata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Darnell Edwards',
+  description: 'Darnell Edwards Portfolio',
 }
 
 export default function RootLayout({
@@ -14,7 +13,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/darnellicon.png" />
+      </head>
       <body>{children}</body>
     </html>
   )
 }
+
